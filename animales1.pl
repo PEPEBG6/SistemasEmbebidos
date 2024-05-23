9be683cbc46561df9f84422d6c9ba2d48613a756
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2024-05-22 20:28:47 -0600

    Actividades

--- animales1.pl
+++ animales1.pl
@@ -1,3 +1,11 @@
 es_gato(wafles).
 es_raton(panzon).
 es_perro(felipe).
+
+masgrande(elefante,caballo).
+masgrande(caballo,perro).
+masgrande(perro,raton).
+
+masgrande(oso,perro,raton).
+
+muchomasgrande(A,C):-masgrande(A,B),masgrande(B,C).
---
+++ progenitor.pl
@@ -0,0 +1,7 @@
+esprogenitor(rosa,maria).
+esprogenitor(rosa,marisol).
+esprogenitor(jorge,abel).
+esprogenitor(abel,rosa).
+esprogenitor(juan,jose).
+esprogenitor(juan,fernanda).
+esprogenitor(manuel,juan).
